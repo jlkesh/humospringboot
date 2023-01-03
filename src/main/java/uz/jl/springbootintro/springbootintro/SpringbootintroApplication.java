1f8ec4c999f13852e4e4e6fb7ec4bb12afc51771
jlkesh <john.lgd65@gmail.com>
2023-01-03 18:41:49 +0500

    initial

--- src/main/java/uz/jl/springbootintro/springbootintro/SpringbootintroApplication.java
+++ src/main/java/uz/jl/springbootintro/springbootintro/SpringbootintroApplication.java
@@ -2,14 +2,6 @@ package uz.jl.springbootintro.springbootintro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 @SpringBootApplication
 public class SpringbootintroApplication {
